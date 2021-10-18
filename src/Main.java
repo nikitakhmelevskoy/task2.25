353d8a06aa56bc030108b66ae19a6f72cb868f86
nikita <nikita03.wot@gmail.com>
2021-10-18 20:54:25 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,38 @@ public class Main {
         checkNum(x);
         int y = readNumber("Введите количество очков команды M = ");
         checkNum(y);
-        if (x > y) {
-            System.out.println("Количество очков команды N = 3, количество очков команды M = 0");
-        }
-        else if (x < y) {
-            System.out.println("Количество очков команды N = 0, количество очков команды M = 3");
-        }
-        else {
-            System.out.println("Количество очков команды N = 1, количество очков команды M = 1");
-        }
-
+        Result end = compareNum(x, y);
+        NumOfPoints(end);
     }
-        static int readNumber(String text){
-            Scanner scanner = new Scanner(System.in);
-            System.out.print(text);
-            return scanner.nextInt();
-        }
 
-        static void checkNum(int num) {
+    static int readNumber(String text) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(text);
+        return scanner.nextInt();
+    }
+
+    static void checkNum(int num) {
         if (num < 0) {
             System.out.print("Ошибка, введите число > 0");
             System.exit(0);
+        }
+    }
+
+    static Result compareNum(int num1, int num2) {
+        if (num1 > num2) {
+            return Result.FirstTeamWin;
+        } else if (num1 < num2) {
+            return Result.FirstTeamLoose;
+        } else {
+            return Result.Draw;
+        }
+    }
+
+    static void NumOfPoints(Result end) {
+        switch (end) {
+            case Draw -> System.out.println("Количество очков команды N = 1, количество очков команды M = 1");
+            case FirstTeamLoose -> System.out.println("Количество очков команды N = 0, количество очков команды M = 3");
+            case FirstTeamWin -> System.out.println("Количество очков команды N = 3, количество очков команды M = 0");
         }
     }
 }
---
+++ src/Result.java
@@ -0,0 +1,5 @@
+public enum Result {
+    FirstTeamWin,
+    FirstTeamLoose,
+    Draw
+}
